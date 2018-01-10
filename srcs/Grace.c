e8a02c435c72ac635ff9f8b3b21ca776282d2c2f
grumbach <agrumbac@student.42.fr>
2018-01-10 17:42:03 +0100

    [ADD] Grace is ungracefully completed

--- srcs/Grace.c
+++ srcs/Grace.c
@@ -1,11 +1,4 @@
-# include <stdio.h>
-# include <fcntl.h>
-# include <unistd.h>
-
-# define USE_ME 0
-# define RUN(x) int main(){x}
 # define CODE \
-	int fd = open("Grace_kid.c", O_CREAT | O_WRONLY);\
 	char *code[] =\
 	{\
 		"Grace_kid.c",\
@@ -13,13 +6,8 @@
 		"		NULL",\
 		"%s%c%c",\
 		"%c%c%c%s%c,%c%c",\
-		"# include <stdio.h>",\
-		"# include <fcntl.h>",\
-		"# include <unistd.h>",\
-		"",\
-		"# define FILENAME ",\
-		"# define RUN(x) int main(){x}",\
-		"# define CODE int fd = open(FILENAME, O_CREAT | O_WRONLY);",\
+		"%s%c",\
+		"# define CODE ",\
 		"	char *code[] =",\
 		"	{",\
 		NULL,\
@@ -27,43 +15,66 @@
 		"	",\
 		"	char	**str = (void *)code;",\
 		"	char	**str_quoted = str;",\
-		"	char	*filename =  str++;",\
+		"	char	*filename = *str++;",\
 		"	char	*null = *str++;",\
 		"	char	*null2 = *str++;",\
 		"	char	*a = *str++;",\
 		"	char	*b = *str++;",\
+		"	char	*c = *str++;",\
+		"	",\
+		"	int fd = open(filename, O_CREAT | O_WRONLY, 0600);",\
 		"	",\
 		"	while (*str)",\
 		"	{",\
-		"		dprintf(fd, a, *str, 92, 10);",\
+		"		dprintf(fd, a, *str, BACKSLASH, 10);",\
 		"		str++;",\
 		"	}",\
 		"	while (*str_quoted)",\
 		"	{",\
-		"		dprintf(fd, b, 9, 9, 34, *str_quoted, 34, 92, 10);",\
+		"		dprintf(fd, b, 9, 9, 34, *str_quoted, 34, BACKSLASH, 10);",\
 		"		str_quoted++;",\
 		"	}",\
+		"	dprintf(fd, a, null, BACKSLASH, 10);",\
 		"	str_quoted++;",\
-		"	puts(null);",\
 		"	while (*str_quoted)",\
 		"	{",\
-		"		dprintf(fd, b, 9, 9, 34, *str_quoted, 34, 92, 10);",\
+		"		dprintf(fd, b, 9, 9, 34, *str_quoted, 34, BACKSLASH, 10);",\
 		"		str_quoted++;",\
 		"	}",\
-		"	puts(null2);",\
+		"	dprintf(fd, a, null, BACKSLASH, 10);",\
+		"	str_quoted++;",\
+		"	while (*str_quoted)",\
+		"	{",\
+		"		dprintf(fd, b, 9, 9, 34, *str_quoted, 34, BACKSLASH, 10);",\
+		"		str_quoted++;",\
+		"	}",\
+		"	dprintf(fd, a, null2, BACKSLASH, 10);",\
 		"	str++;",\
 		"	while (*str)",\
 		"	{",\
-		"		dprintf(fd, a, *str, 92, 10);",\
+		"		dprintf(fd, a, *str, BACKSLASH, 10);",\
 		"		str++;",\
 		"	}",\
-		"	",\
+		"	str++;",\
+		"	while (*str)",\
+		"	{",\
+		"		dprintf(fd, c, *str, 10);",\
+		"		str++;",\
+		"	}",\
+		NULL,\
 		"	close(fd);",\
 		"",\
 		"/*",\
-		"	This is getting ugly...",\
+		"	This is trully UGLY...",\
 		"*/",\
-		"	",\
+		"",\
+		"# include <stdio.h>",\
+		"# include <fcntl.h>",\
+		"# include <unistd.h>",\
+		"",\
+		"# define BACKSLASH	92",\
+		"# define RUN(x) int main(){x}",\
+		"",\
 		"RUN(CODE)",\
 		NULL\
 	};\
@@ -75,38 +86,58 @@
 	char	*null2 = *str++;\
 	char	*a = *str++;\
 	char	*b = *str++;\
+	char	*c = *str++;\
 	\
-	(void)filename;\
+	int fd = open(filename, O_CREAT | O_WRONLY, 0600);\
 	\
 	while (*str)\
 	{\
-		dprintf(fd, a, *str, 92, 10);\
+		dprintf(fd, a, *str, BACKSLASH, 10);\
 		str++;\
 	}\
 	while (*str_quoted)\
 	{\
-		dprintf(fd, b, 9, 9, 34, *str_quoted, 34, 92, 10);\
+		dprintf(fd, b, 9, 9, 34, *str_quoted, 34, BACKSLASH, 10);\
 		str_quoted++;\
 	}\
+	dprintf(fd, a, null, BACKSLASH, 10);\
 	str_quoted++;\
-	dprintf(fd, a, null, 92, 10);\
 	while (*str_quoted)\
 	{\
-		dprintf(fd, b, 9, 9, 34, *str_quoted, 34, 92, 10);\
+		dprintf(fd, b, 9, 9, 34, *str_quoted, 34, BACKSLASH, 10);\
 		str_quoted++;\
 	}\
-	dprintf(fd, a, null2, 92, 10);\
+	dprintf(fd, a, null, BACKSLASH, 10);\
+	str_quoted++;\
+	while (*str_quoted)\
+	{\
+		dprintf(fd, b, 9, 9, 34, *str_quoted, 34, BACKSLASH, 10);\
+		str_quoted++;\
+	}\
+	dprintf(fd, a, null2, BACKSLASH, 10);\
 	str++;\
 	while (*str)\
 	{\
-		dprintf(fd, a, *str, 92, 10);\
+		dprintf(fd, a, *str, BACKSLASH, 10);\
 		str++;\
 	}\
-	\
+	str++;\
+	while (*str)\
+	{\
+		dprintf(fd, c, *str, 10);\
+		str++;\
+	}\
 	close(fd);
 
 /*
 	This is trully UGLY...
 */
+
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+# define BACKSLASH	92
+# define RUN(x) int main(){x}
 
 RUN(CODE)
