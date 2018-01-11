0e5a7ce744d242718a78c6cd7c48c7aad6dc4ae6
grumbach <agrumbac@student.42.fr>
2018-01-11 22:50:50 +0100

    [FIX] fixed C sully (skipped sully_5)

--- srcs/Sully.c
+++ srcs/Sully.c
@@ -2,14 +2,15 @@
 # include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
-# define CODE_HEAD	6
+# include <string.h>
+# define CODE_HEAD	4
 
 void			replicate(char **code, const int x)
 {
-	const int	fd			= open(code[0], O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	const char	*line		= code[2];
-	const char	*quoted		= code[3];
-	const char	*variable_x	= code[4];
+	const int	fd			= open((char[10]){'S', 'u', 'l', 'l', 'y', '_', (48 + x - 1), '.', 'c', 0}, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	const char	*line		= code[0];
+	const char	*quoted		= code[1];
+	const char	*variable_x	= code[2];
 	int			i;
 
 	i = CODE_HEAD;
@@ -18,11 +19,10 @@ void			replicate(char **code, const int x)
 	dprintf(fd, variable_x, (x - 1), 10);//setting X
 	dprintf(fd, line, code[i], 10);//two more lines
 	dprintf(fd, line, code[++i], 10);
-	dprintf(fd, code[5], 9, 9, code[1], 10);//the ()char[10] line
-	i = 0;
+	i = -1;
 	while (++i < 51)
 		dprintf(fd, quoted, 9, 9, 34, code[i], 34, 44, 10);//quoted code
-	dprintf(fd, code[5], 9, 9, code[6], 10);//the NULL line
+	dprintf(fd, code[3], 9, 9, code[4], 10);//the NULL line
 	i = 38 + CODE_HEAD;
 	while (++i < 51)
 		dprintf(fd, line, code[i], 10);//code lines end
@@ -37,8 +37,6 @@ int				main(void)
 	int			X = 5;
 	char		*code[] =
 	{
-		(char[10]){'S', 'u', 'l', 'l', 'y', '_', (48 + X - 1), '.', 'c', 0},
-		"(char[10]){'S', 'u', 'l', 'l', 'y', '_', (48 + X - 1), '.', 'c', 0},",
 		"%s%c",
 		"%c%c%c%s%c%c%c",
 		"	int			X = %d;%c",
@@ -48,14 +46,15 @@ int				main(void)
 		"# include <fcntl.h>",
 		"# include <unistd.h>",
 		"# include <stdlib.h>",
-		"# define CODE_HEAD	6",
+		"# include <string.h>",
+		"# define CODE_HEAD	4",
 		"",
 		"void			replicate(char **code, const int x)",
 		"{",
-		"	const int	fd			= open(code[0], O_CREAT | O_WRONLY | O_TRUNC, 0600);",
-		"	const char	*line		= code[2];",
-		"	const char	*quoted		= code[3];",
-		"	const char	*variable_x	= code[4];",
+		"	const int	fd			= open((char[10]){'S', 'u', 'l', 'l', 'y', '_', (48 + x - 1), '.', 'c', 0}, O_CREAT | O_WRONLY | O_TRUNC, 0600);",
+		"	const char	*line		= code[0];",
+		"	const char	*quoted		= code[1];",
+		"	const char	*variable_x	= code[2];",
 		"	int			i;",
 		"",
 		"	i = CODE_HEAD;",
@@ -64,11 +63,10 @@ int				main(void)
 		"	dprintf(fd, variable_x, (x - 1), 10);//setting X",
 		"	dprintf(fd, line, code[i], 10);//two more lines",
 		"	dprintf(fd, line, code[++i], 10);",
-		"	dprintf(fd, code[5], 9, 9, code[1], 10);//the ()char[10] line",
-		"	i = 0;",
+		"	i = -1;",
 		"	while (++i < 51)",
 		"		dprintf(fd, quoted, 9, 9, 34, code[i], 34, 44, 10);//quoted code",
-		"	dprintf(fd, code[5], 9, 9, code[6], 10);//the NULL line",
+		"	dprintf(fd, code[3], 9, 9, code[4], 10);//the NULL line",
 		"	i = 38 + CODE_HEAD;",
 		"	while (++i < 51)",
 		"		dprintf(fd, line, code[i], 10);//code lines end",
@@ -84,14 +82,18 @@ int				main(void)
 		"	{",
 		"	};",
 		"",
-		"	if (X)",
+		"	if (X > 0 && strstr(__FILE__, (char[8]){'S','u','l','l','y','.','c',0}))",
+		"		replicate(code, X + 1);",
+		"	else if (X > 0)",
 		"		replicate(code, X);",
 		"	return (0);",
 		"}",
 		NULL
 	};
 
-	if (X)
+	if (X > 0 && strstr(__FILE__, (char[8]){'S','u','l','l','y','.','c',0}))
+		replicate(code, X + 1);
+	else if (X > 0)
 		replicate(code, X);
 	return (0);
 }
