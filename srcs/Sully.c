aae6bfe156fd5d4e2a105f5fa57a3181227bcf61
grumbach <agrumbac@student.42.fr>
2018-01-13 15:02:53 +0100

    [FIX] sully now accepts any int for X

--- srcs/Sully.c
+++ srcs/Sully.c
@@ -5,31 +5,47 @@
 # include <string.h>
 # define CODE_HEAD	4
 
-void			replicate(char **code, const int x)
+char			*make_string(char *buf, const int x, const int name_only)
 {
-	const int	fd			= open((char[10]){'S', 'u', 'l', 'l', 'y', '_', (48 + x - 1), '.', 'c', 0}, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	const char	*sully	= (char[7]){'S', 'u', 'l', 'l', 'y', '_', 0};
+	const char	*dotc	= (char[3]){'.', 'c', 0};
+	const char	*clang	= (char[7]){'c', 'l', 'a', 'n', 'g', ' ', 0};
+	const char	*flags	= (char[5]){' ','-','o',' ', 0};
+	const char	*andrun	= (char[6]){'&', '&' ,' ' ,'.' ,'/' , 0};
+	const char	*fmt	= (char[7]){'%', 's', '%', 'd', '%', 's', 0};
+	const char	*fmt2	= (char[21]){'%', 's', '%', 's', '%', 'd', '%', 's', '%', 's', '%', 's', '%', 'd', '%', 's', '%', 's', '%', 'd', 0};
+
+	if (name_only)
+		sprintf(buf, fmt, sully, x, dotc);
+	else
+		sprintf(buf, fmt2, clang, sully, x, dotc, flags, sully, x, andrun, sully, x);
+	return (buf);
+}
+
+void			replicate(char **code, const unsigned int x)
+{
+	char		buf[64];
+	const int	fd			= open(make_string(buf, x - 1, 1), O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	const char	*line		= code[0];
 	const char	*quoted		= code[1];
 	const char	*variable_x	= code[2];
 	int			i;
 
 	i = CODE_HEAD;
-	while (++i < 37 + CODE_HEAD)
+	while (++i < 53 + CODE_HEAD)
 		dprintf(fd, line, code[i], 10);//code lines beginning
 	dprintf(fd, variable_x, (x - 1), 10);//setting X
 	dprintf(fd, line, code[i], 10);//two more lines
 	dprintf(fd, line, code[++i], 10);
 	i = -1;
-	while (++i < 51)
+	while (++i < 67)
 		dprintf(fd, quoted, 9, 9, 34, code[i], 34, 44, 10);//quoted code
 	dprintf(fd, code[3], 9, 9, code[4], 10);//the NULL line
-	i = 38 + CODE_HEAD;
-	while (++i < 51)
+	i = 54 + CODE_HEAD;
+	while (++i < 67)
 		dprintf(fd, line, code[i], 10);//code lines end
 	close(fd);
-	system((char[27]){'c','l','a','n','g',' ','S','u','l','l','y','_',47 + x,
-		'.','c',' ','-','o',' ','S','u','l','l','y','_',47 + x,0});
-	system((char[10]){'.','/','S','u','l','l','y','_',47 + x,0});
+	system(make_string(buf, x - 1, 0));
 }
 
 int				main(void)
@@ -49,31 +65,47 @@ int				main(void)
 		"# include <string.h>",
 		"# define CODE_HEAD	4",
 		"",
-		"void			replicate(char **code, const int x)",
+		"char			*make_string(char *buf, const int x, const int name_only)",
 		"{",
-		"	const int	fd			= open((char[10]){'S', 'u', 'l', 'l', 'y', '_', (48 + x - 1), '.', 'c', 0}, O_CREAT | O_WRONLY | O_TRUNC, 0600);",
+		"	const char	*sully	= (char[7]){'S', 'u', 'l', 'l', 'y', '_', 0};",
+		"	const char	*dotc	= (char[3]){'.', 'c', 0};",
+		"	const char	*clang	= (char[7]){'c', 'l', 'a', 'n', 'g', ' ', 0};",
+		"	const char	*flags	= (char[5]){' ','-','o',' ', 0};",
+		"	const char	*andrun	= (char[6]){'&', '&' ,' ' ,'.' ,'/' , 0};",
+		"	const char	*fmt	= (char[7]){'%', 's', '%', 'd', '%', 's', 0};",
+		"	const char	*fmt2	= (char[21]){'%', 's', '%', 's', '%', 'd', '%', 's', '%', 's', '%', 's', '%', 'd', '%', 's', '%', 's', '%', 'd', 0};",
+		"",
+		"	if (name_only)",
+		"		sprintf(buf, fmt, sully, x, dotc);",
+		"	else",
+		"		sprintf(buf, fmt2, clang, sully, x, dotc, flags, sully, x, andrun, sully, x);",
+		"	return (buf);",
+		"}",
+		"",
+		"void			replicate(char **code, const unsigned int x)",
+		"{",
+		"	char		buf[64];",
+		"	const int	fd			= open(make_string(buf, x - 1, 1), O_CREAT | O_WRONLY | O_TRUNC, 0600);",
 		"	const char	*line		= code[0];",
 		"	const char	*quoted		= code[1];",
 		"	const char	*variable_x	= code[2];",
 		"	int			i;",
 		"",
 		"	i = CODE_HEAD;",
-		"	while (++i < 37 + CODE_HEAD)",
+		"	while (++i < 53 + CODE_HEAD)",
 		"		dprintf(fd, line, code[i], 10);//code lines beginning",
 		"	dprintf(fd, variable_x, (x - 1), 10);//setting X",
 		"	dprintf(fd, line, code[i], 10);//two more lines",
 		"	dprintf(fd, line, code[++i], 10);",
 		"	i = -1;",
-		"	while (++i < 51)",
+		"	while (++i < 67)",
 		"		dprintf(fd, quoted, 9, 9, 34, code[i], 34, 44, 10);//quoted code",
 		"	dprintf(fd, code[3], 9, 9, code[4], 10);//the NULL line",
-		"	i = 38 + CODE_HEAD;",
-		"	while (++i < 51)",
+		"	i = 54 + CODE_HEAD;",
+		"	while (++i < 67)",
 		"		dprintf(fd, line, code[i], 10);//code lines end",
 		"	close(fd);",
-		"	system((char[27]){'c','l','a','n','g',' ','S','u','l','l','y','_',47 + x,",
-		"		'.','c',' ','-','o',' ','S','u','l','l','y','_',47 + x,0});",
-		"	system((char[10]){'.','/','S','u','l','l','y','_',47 + x,0});",
+		"	system(make_string(buf, x - 1, 0));",
 		"}",
 		"",
 		"int				main(void)",
